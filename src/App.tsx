8546541f17f5ebcaa20e86ad1675ec4869517767
Suraj Yadav <ysuraj877@gmail.com>
2022-08-11 22:14:43 +0530

    added datatable

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import DataTable from 'react-data-table-component';
+import { fetch } from './functions/fetch';
 import './App.css';
+import CSV from './classes/CSV';
 
 function App() {
+  const [readable, setReadable] = useState([] as any)
+
+  function concatReadable(anotherReadable: Array<any>){
+    const newReadable = [...readable, ...anotherReadable]
+    setReadable(() => newReadable)
+  }
+
+  const columns = [
+    {
+      name: 'Title',
+      selector: (row: any) => row.title,
+      sortable: true
+    },
+    {
+      name: 'ISBN',
+      selector: (row: any) => row.isbn,
+    },
+    {
+      name: 'Author',
+      selector: (row: any) => row.authors,
+    }
+  ];
+
+  useEffect(() => {
+    fetch("https://raw.githubusercontent.com/echocat/nodejs-kata-1/master/data/magazines.csv")
+      .then(x => {
+        const csv = new CSV(x)
+        concatReadable(csv.toJson())
+      })
+  }, [])
+
+  useEffect(() => {
+    fetch("https://raw.githubusercontent.com/echocat/nodejs-kata-1/master/data/books.csv")
+      .then(x => {
+        const csv = new CSV(x)
+        concatReadable(csv.toJson())
+      })
+  }, [])
+
   return (
-    <div></div>
+    <>
+      <pre>{JSON.stringify(readable, null, 4)}</pre>
+      {readable && <DataTable columns={columns} data={readable} dense highlightOnHover pointerOnHover/>}
+    </>
   );
 }
 
