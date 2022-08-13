a71b205f234aea90611faa92922aabbfe1af1139
Suraj Yadav <ysuraj877@gmail.com>
2022-08-13 22:16:45 +0530

    added fuzzy finding

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import DataTable from 'react-data-table-component';
 import { fetch } from './functions/fetch';
 import './App.css';
@@ -9,6 +9,7 @@ import BookAction from './types/BookAction';
 import MagazineAction from './types/MagazineAction';
 import AuthorAction from './types/AuthorAction';
 import csvToJson from './functions/csvToJson';
+import fuzzysearch from 'fuzzysearch-ts';
 
 function authorReducer(state: Author[], action: AuthorAction){
   switch(action.type){
@@ -41,6 +42,16 @@ function App() {
   const [author, authorDispatch] = useReducer(authorReducer, [])
   const [magazine, magazineDispatch] = useReducer(magazineReducer, [])
   const [book, bookDispatch] = useReducer(bookReducer, [])
+  const [search, setSearch] = useState("")
+  const searchKeys = ["title", "isbn", "authors"]
+
+  function searchBooks(needle: string, haystack: Array<any>){
+    return haystack.filter(item => {
+      const anyPresent = searchKeys.map(key => fuzzysearch(needle.toLowerCase(), item[key].toLowerCase()))
+      .reduce((a: boolean, b: boolean) => a || b)
+      return anyPresent
+    })
+  }
 
   /**
    * fetch magazines on initial render
@@ -94,10 +105,15 @@ function App() {
   return (
     <div className='container'>
       <div className='searchContainer'>
-        <input type="text" className='search' />
+        <input type="text" className='search' onChange={e => setSearch(e.target.value)}/>
       </div>
       <div className='tableContainer'>
-        <DataTable columns={columns} data={(book as Array<any>).concat(magazine as Array<any>)} dense highlightOnHover pointerOnHover />
+        {
+          search ?
+          <DataTable columns={columns} data={searchBooks(search, (book as Array<any>).concat(magazine as Array<any>))} dense highlightOnHover pointerOnHover />
+          :
+          <DataTable columns={columns} data={(book as Array<any>).concat(magazine as Array<any>)} dense highlightOnHover pointerOnHover />
+        }
       </div>
     </div>
   );
