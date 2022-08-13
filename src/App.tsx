28ed8f639d4332176eca563cadd082524adcf9d3
Suraj Yadav <ysuraj877@gmail.com>
2022-08-13 18:32:07 +0530

    replace readable with reducers

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,60 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import DataTable from 'react-data-table-component';
 import { fetch } from './functions/fetch';
 import './App.css';
 import CSV from './classes/CSV';
+import Author from './types/Author';
+import Book from './types/Book';
+import Magazine from './types/Magazine';
+import BookAction from './types/BookAction';
+import MagazineAction from './types/MagazineAction';
+import AuthorAction from './types/AuthorAction';
+
+function authorReducer(state: Author[], action: AuthorAction){
+  switch(action.type){
+    default:
+      return state
+  }
+}
+
+function bookReducer(state: Book[], action: BookAction){
+  switch(action.type){
+    case "FETCH":
+      return action.payload
+    default:
+      return state
+  }
+}
+
+function magazineReducer(state: Magazine[], action: MagazineAction){
+  switch(action.type){
+    case "FETCH":
+      return action.payload
+    default:
+      return state
+  }
+}
 
 function App() {
-  const [readable, setReadable] = useState([] as any)
+  const [author, authorDispatch] = useReducer(authorReducer, [])
+  const [magazine, magazineDispatch] = useReducer(magazineReducer, [])
+  const [book, bookDispatch] = useReducer(bookReducer, [])
 
-  function concatReadable(anotherReadable: Array<any>){
-    const newReadable = [...readable, ...anotherReadable]
-    setReadable(() => newReadable)
-  }
+  useEffect(() => {
+    fetch("https://raw.githubusercontent.com/echocat/nodejs-kata-1/master/data/magazines.csv")
+      .then(x => {
+        const csv = new CSV(x)
+        magazineDispatch({type: "FETCH", payload: csv.toJson()})
+      })
+  }, [])
+
+  useEffect(() => {
+    fetch("https://raw.githubusercontent.com/echocat/nodejs-kata-1/master/data/books.csv")
+      .then(x => {
+        const csv = new CSV(x)
+        bookDispatch({type: "FETCH", payload: csv.toJson()})
+      })
+  }, [])
 
   const columns = [
     {
@@ -28,27 +72,13 @@ function App() {
     }
   ];
 
-  useEffect(() => {
-    fetch("https://raw.githubusercontent.com/echocat/nodejs-kata-1/master/data/magazines.csv")
-      .then(x => {
-        const csv = new CSV(x)
-        concatReadable(csv.toJson())
-      })
-  }, [])
-
-  useEffect(() => {
-    fetch("https://raw.githubusercontent.com/echocat/nodejs-kata-1/master/data/books.csv")
-      .then(x => {
-        const csv = new CSV(x)
-        concatReadable(csv.toJson())
-      })
-  }, [])
-
   return (
-    <>
-      <pre>{JSON.stringify(readable, null, 4)}</pre>
-      {readable && <DataTable columns={columns} data={readable} dense highlightOnHover pointerOnHover/>}
-    </>
+    <div className='container'>
+      <div className='searchContainer'>
+        <input type="text" className='search' />
+      </div>
+      {(book || magazine) && <DataTable columns={columns} data={(book as Array<any>).concat(magazine as Array<any>)} dense highlightOnHover pointerOnHover />}
+    </div>
   );
 }
 
